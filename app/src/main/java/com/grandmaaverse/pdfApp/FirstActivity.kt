c1d3dcb422be822853dfa21b749c357d1e7bf5f1
Rao Aaraf Younus <aarafrao22@gmail.com>
2022-06-25 22:58:00 +0500

    100. complete

--- app/src/main/java/com/grandmaaverse/pdfApp/FirstActivity.kt
+++ app/src/main/java/com/grandmaaverse/pdfApp/FirstActivity.kt
@@ -1,14 +1,20 @@
 package com.grandmaaverse.pdfApp
 
+import android.content.ActivityNotFoundException
+import android.content.ComponentName
 import android.content.ContentValues.TAG
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.ResolveInfo
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.ads.*
 import com.google.android.gms.ads.interstitial.InterstitialAd
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import com.grandmaaverse.pdfApp.databinding.ActivityFirstBinding
+
 
 class FirstActivity : AppCompatActivity() {
     private var mInterstitialAd: InterstitialAd? = null
@@ -24,7 +30,7 @@ class FirstActivity : AppCompatActivity() {
 
         InterstitialAd.load(
             this,
-            "ca-app-pub-3940256099942544/1033173712",
+            "ca-app-pub-8620621477902075/2069561655",
             adRequest1,
             object : InterstitialAdLoadCallback() {
                 override fun onAdFailedToLoad(adError: LoadAdError) {
@@ -65,7 +71,6 @@ class FirstActivity : AppCompatActivity() {
             }
         }
 
-
         val mAdView: AdView
         MobileAds.initialize(this) {}
         mAdView = findViewById(R.id.adView2)
@@ -84,6 +89,67 @@ class FirstActivity : AppCompatActivity() {
 
         }
 
+        binding.btnReview.setOnClickListener {
+            openAppRating(applicationContext)
+        }
 
+        binding.btnInsta.setOnClickListener {
+            openInsta()
+        }
+
+
+    }
+
+    private fun openInsta() {
+        val uri = Uri.parse("https://www.instagram.com/psychologist_kashif/")
+        val likeIng = Intent(Intent.ACTION_VIEW, uri)
+        likeIng.setPackage("com.instagram.android")
+        try {
+            startActivity(likeIng)
+        } catch (e: ActivityNotFoundException) {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://www.instagram.com/psychologist_kashif/")
+                )
+            )
+        }
+    }
+
+    fun openAppRating(context: Context) {
+        val appId: String = context.getPackageName()
+        val rateIntent = Intent(
+            Intent.ACTION_VIEW,
+            Uri.parse("market://details?id=$appId")
+        )
+        var marketFound = false
+        val otherApps: List<ResolveInfo> = context.getPackageManager()
+            .queryIntentActivities(rateIntent, 0)
+        for (otherApp in otherApps) {
+            if (otherApp.activityInfo.applicationInfo.packageName
+                == "com.android.vending"
+            ) {
+                val otherAppActivity = otherApp.activityInfo
+                val componentName = ComponentName(
+                    otherAppActivity.applicationInfo.packageName,
+                    otherAppActivity.name
+                )
+                rateIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                rateIntent.addFlags(Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED)
+                rateIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                rateIntent.component = componentName
+                context.startActivity(rateIntent)
+                marketFound = true
+                break
+            }
+        }
+
+        if (!marketFound) {
+            val webIntent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://play.google.com/store/apps/details?id=$appId")
+            )
+            context.startActivity(webIntent)
+        }
     }
 }
